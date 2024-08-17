7e8381d8e1ca12779fda12b2572b0914fb5bd018
Bill Weaver <wweave42@gmail.com>
2024-08-17 15:32:33 -0400

    Create script.py

---
+++ script.py
@@ -0,0 +1,8 @@
+import codecademylib3
+import pandas as pd
+
+visits = pd.read_csv('visits.csv', parse_dates=[1])
+cart = pd.read_csv('cart.csv', parse_dates=[1])
+checkout = pd.read_csv('checkout.csv', parse_dates=[1])
+purchase = pd.read_csv('purchase.csv', parse_dates=[1])
+
